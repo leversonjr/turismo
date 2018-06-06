26b1a6651aab6970a454c2162726606cbdc6fcd9
leversonjr <38119912+leversonjr@users.noreply.github.com>
2018-06-05 22:56:36 -0300

    ''

--- turismo/settings.py
+++ turismo/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Minhas Aplicaçôes
+    'turismos',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# E-MAIL
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #imprime no console
+EMAIL_HOST = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = 'vitoriaturismogoias@gmail.com'
--- turismos/views.py
+++ turismos/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+# Create your views here
+
+def index(request):
+    ''' Pagina inicial vitoria turismo '''
+    return render(request, 'turismos/index.html')
+
+def contato(request):
+    sucesso: False
+    if request.method == 'POST':
+        contato_form = ContatoForm(request.POST)
+        if contato_form.is_valid():
+            nome = contato_form.cleaned_data['nome']
+            email = contato_form.cleaned_data['email']
+            mensagem = contato_form.cleaned_data['mensagem']
+            mensagem = 'None: {0} \E-mail: {1}\n{2}'.format(nome, email, mensagem)
+            send_mail(
+                'Contato Do Leverson teste', mensagem, settings.DEFAULT_FROM_EMAIL,
+                [settings.DEFAULT_FROM_EMAIL]
+            )
+            sucesso = True
+
+        else:
+            contato_form = ContatoForm()
+            context = {
+                'contato_form': contato_form,
+                'sucesso': sucesso
+            }
+            return render(request, 'turismos/contato.html', context)

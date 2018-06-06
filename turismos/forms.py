a3b9bf5a0d18f01cf2574163520f42d1d409641a
leversonjr <38119912+leversonjr@users.noreply.github.com>
2018-06-05 23:41:25 -0300

    ''

---
+++ turismos/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class ContatoForm(forms):
+
+    nome = forms.CharField(label='Nome', required=True)
+    email = forms.EmailField(label='Email')
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea(), required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(ContatoForm, self).__init__(*args, **kwargs)
+        self.fields['nome'].widget.attrs['class'] = 'form-control form-control-sm'
+        self.fields['nome'].widget.attrs['placeholder'] = 'Digite o seu nome completo'
+        self.fields['email'].widget.attrs['class'] = 'form-control form-control-sm'
+        self.fields['email'].widget.attrs['placeholder'] = 'Digite seu email'
+        self.fields['mensagem'].widget.attrs['class'] = 'form-control form-control-sm'
+        self.fields['mensagem'].widget.attrs['placeholder'] = 'Escreva sua mensagem...'

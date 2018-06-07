8aced840efc19506f20c45af71335d3c3d82ed99
leversonjr <38119912+leversonjr@users.noreply.github.com>
2018-06-06 23:56:08 -0300

    ''

---
+++ turismos/urls.py
@@ -0,0 +1,17 @@
+'''Define padrões de url para Turismos'''
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    #Pagina Inicial
+    path('', views.index, name='index'),
+
+    #Pagina de Contato
+    path('contato/', views.contato, name='contato'),
+
+# Pagina sobre a Empresa
+    path('sobre/', views.sobre, name='sobre'),
+
+]
--- turismos/views.py
+++ turismos/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
+from .forms import ContatoForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.template import RequestContext
 
 # Create your views here
 
@@ -8,25 +12,34 @@ def index(request):
     ''' Pagina inicial vitoria turismo '''
     return render(request, 'turismos/index.html')
 
+def sobre(request):
+    ''' Pagina sobre a empresa '''
+    return render(request, 'turismos/sobre.html')
+
 def contato(request):
-    sucesso: False
+
+    sucesso = False
     if request.method == 'POST':
         contato_form = ContatoForm(request.POST)
         if contato_form.is_valid():
             nome = contato_form.cleaned_data['nome']
             email = contato_form.cleaned_data['email']
             mensagem = contato_form.cleaned_data['mensagem']
-            mensagem = 'None: {0} \E-mail: {1}\n{2}'.format(nome, email, mensagem)
+            mensagem = 'Nome: {0} \E-mail: {1}\n{2}'.format(nome, email, mensagem)
             send_mail(
                 'Contato Do Leverson teste', mensagem, settings.DEFAULT_FROM_EMAIL,
                 [settings.DEFAULT_FROM_EMAIL]
             )
+
             sucesso = True
+            print('CONTATO_FORM  E VALIDO!!!!')
 
         else:
+            print('CONTATO_FORM E INVALIDO!!!!')
             contato_form = ContatoForm()
-            context = {
-                'contato_form': contato_form,
-                'sucesso': sucesso
-            }
-            return render(request, 'turismos/contato.html', context)
+        context = {'contato_form': contato_form}
+        return render(request, 'turismos/contato.html', context)
+    else:
+        contato_form = ContatoForm()
+        context = {'contato_form': contato_form}
+        return render(request, 'turismos/contato.html', context)
